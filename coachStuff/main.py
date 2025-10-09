f14853afbfa9f00465fab64f4e40c685969b67de
izz13 <94653804+izz13@users.noreply.github.com>
2025-10-08 19:48:04 -0700

    Add GIF animations and update main.py for animation support
    Added three GIF files for animations and updated main.py to load and display an animated GIF using a timer. The draw function now handles animation frames and accepts a dt parameter for frame timing.

--- coachStuff/main.py
+++ coachStuff/main.py
@@ -14,13 +14,28 @@ g = Grid([WIDTH,HEIGHT])
 
 kevinSound = pygame.mixer.Sound("kevinSound.wav")
 
-
+manAnimation = pygame.image.load_animation("coachStuff/05 zx .gif")
+print(manAnimation)
+aniTimer = 0
+aniCooldown = 1/15
+index = 0
+dt = 0
 
 soundPlay = False
 
-def draw(screen):
+def draw(screen,dt):
+    global aniTimer,aniCooldown,index
     screen.fill("grey")
-    g.draw(screen)
+    length = len(manAnimation)
+    frame = manAnimation[index%length][0]
+    screen.blit(frame,frame.get_rect(center = [WIDTH/2,HEIGHT/2]))
+    if aniTimer > aniCooldown:
+        index += 1
+        aniTimer = 0
+    else:
+        aniTimer += dt
+    print(aniTimer)
+    #g.draw(screen)
 
 
 def update():
@@ -36,9 +51,8 @@ while isRunning:
     for event in events:
         if event.type == pygame.QUIT:
             isRunning = False
-
     update()
-    draw(screen)
+    draw(screen,dt)
     dt = clock.tick(fps)/1000
     pygame.display.update()
 pygame.quit()
